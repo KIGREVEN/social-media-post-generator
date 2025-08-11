461b65c438efe2532d9ddfc13ab8ba3a0605ba15
KIGREVEN <kigreven@github.com>
2025-08-11 10:13:15 -0400

    Alternative Subscription-Update-Lösung implementiert

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import DashboardPage from './pages/DashboardPage'
 import PostGeneratorPage from './pages/PostGeneratorPage'
 import PostsPage from './pages/PostsPage'
 import SocialAccountsPage from './pages/SocialAccountsPage'
-import AdminPageFinal from './pages/AdminPageFinal'
+import AdminPageWorking from './pages/AdminPageWorking'
 import ProtectedRoute from './components/ProtectedRoute'
 import './App.css'
 
@@ -60,7 +60,7 @@ function App() {
                 path="/admin" 
                 element={
                   <ProtectedRoute requireAdmin={true}>
-                    <AdminPageFinal />
+                    <AdminPageWorking />
                   </ProtectedRoute>
                 } 
               />
